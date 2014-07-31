a99f4f4f7fe5a3a9d1c9808f73a64d51cc06c0ce
Sarah Kim <skkim@whatever>
2014-07-31 14:40:00 -0700

    added firebase

--- Pinterest.js
+++ Pinterest.js
@@ -1,53 +1,70 @@
 //links from HTML//
-var app = angular.module("pinApp",[]);
+var app = angular.module("pinApp",["firebase"]);
 
 //finds specific controller and links from HTML//
-app.controller("pinController", function($scope){
+app.controller("pinController", function($scope,$firebase){
 
-//the "pins" in 'pin in pins//
-$scope.pins = [
-	{
-		"Picture":"pictures/pic1.jpg" ,
-		"Link":"www.wines.com",
-		"Description": "what is your favorite thing about wine?"
-	},
-	{
-		"Picture":"{{picture}}",
-		"Link":"{{link}}",
-		"Description": "blah"
-	},
-	{
-		"Picture":"{{picture}}",
-		"Link":"{{link}}",
-		"Description": "blah"
-	},
-	{
-		"Picture":"{{picture}}",
-		"Link":"{{link}}",
-		"Description": "blah"
-	},
-	{
-		"Picture":"{{picture}}",
-		"Link":"{{link}}",
-		"Description": "blah"
-	},
-	{
-		"Picture":"{{picture}}",
-		"Link":"{{link}}",
-		"Description": "blah"
-	},
-	{
-		"Picture":"{{picture}}",
-		"Link":"{{link}}",
-		"Description": "blah"
-	},
-	{
-		"Picture":"{{picture}}",
-		"Link":"{{link}}",
-		"Description": "blah"
-	}
+// links Javascript to firebase
+var ref = new Firebase("https://travelist.firebaseio.com/");
+var sync = $firebase(ref);
 
+// function that links information in html to firebase //
 
-]
+$scope.movies = sync.$asArray();
+	$scope.addPin = function(picture, website, description){
+    $scope.pins.$add({picture: picture, website: website, description: description})
+
+  }
+
+ 
 });
+
+// //the "pins" in 'pin in pins//
+// $scope.pins = [
+// 	{
+// 		"Picture":"pictures/beach.jpg",
+// 		"Link":"www.louis.com",
+// 		"Description": "blah"
+// 	},
+// 	{
+// 		"Picture":"pictures/world.jpg",
+// 		"Link":"{{link}}",
+// 		"Description": "blah"
+// 	},
+// 	{
+// 		"Picture":"pictures/lewis.png",
+// 		"Link":"{{link}}",
+// 		"Description": "blah"
+// 	},
+// 	{
+// 		"Picture":"pictures/book.jpg",
+// 		"Link":"{{link}}",
+// 		"Description": "blah"
+// 	},
+// 	{
+// 		"Picture":"pictures/hotair.jpg",
+// 		"Link":"{{link}}",
+// 		"Description": "blah"
+// 	},
+// 	{
+// 		"Picture":"pictures/hike.jpg",
+// 		"Link":"{{link}}",
+// 		"Description": "blah"
+// 	},
+// 	{
+// 		"Picture":"pictures/map.jpg",
+// 		"Link":"{{link}}",
+// 		"Description": "blah"
+// 	},
+// 	{
+// 		"Picture":"pictures/aware.jpg",
+// 		"Link":"{{link}}",
+// 		"Description": "blah"
+// 	}
+
+
+
+
+// ]
+// });
 
